7baf1941f808d3c352a179e84fac7cdff4f4467d
Sundram <sundrraam@gmail.com>
2023-10-15 19:45:38 +0530

    m30s-common: Going back to keymaster 3

--- common.mk
+++ common.mk
@@ -145,8 +145,9 @@ PRODUCT_COPY_FILES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@4.0-service.samsung \
-    libkeymaster4_1support.vendor:64
+    android.hardware.keymaster@3.0.vendor \
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
 
 # Lights
 PRODUCT_PACKAGES += \
