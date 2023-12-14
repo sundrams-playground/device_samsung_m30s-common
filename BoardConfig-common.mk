bdda79a603a1c0648e20e5460ad917e146e54a7b
Sundram <sundrraam@gmail.com>
2023-12-14 19:07:45 +0530

    Revert "m30s-common: Add FlashControl"
    This reverts commit a00a08f3ddc123a09b284af46797fa37bf89cac8.

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -136,7 +136,6 @@ include device/lineage/sepolicy/exynos/sepolicy.mk
 include device/samsung_slsi/sepolicy/sepolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private hardware/samsung-ext/interfaces/sepolicy/private
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
