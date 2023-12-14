68651aa4c056febdf9d0fc8193efa6c55b874b2e
Sundram <sundrraam@gmail.com>
2023-12-14 19:12:38 +0530

    m30s-common: include evo sepolicy

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -132,7 +132,7 @@ VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # SELinux
 BOARD_SEPOLICY_TEE_FLAVOR := teegris
-include device/lineage/sepolicy/exynos/sepolicy.mk
+include device/evolution/sepolicy/exynos/sepolicy.mk
 include device/samsung_slsi/sepolicy/sepolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
