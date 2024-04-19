f5f55c3a50ed7ba5c8c01123ba45f9d4d1630f11
xyz-sundram <sundrraam@gmail.com>
2024-04-19 21:47:56 +0530

    m30s-common: switch to aidl fingerprint hal

--- common.mk
+++ common.mk
@@ -50,7 +50,7 @@ PRODUCT_COPY_FILES += \
 
 # Biometric Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.samsung
+    android.hardware.biometrics.fingerprint-service.samsung
 
 # Bluetooth
 PRODUCT_PACKAGES += \
