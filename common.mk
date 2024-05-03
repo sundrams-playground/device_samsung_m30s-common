c74e315f760821761cb65ef6c386498f4688b71d
xyz-sundram <sundrraam@gmail.com>
2024-05-03 09:25:23 +0530

    m30s-common: bring back remove packages

--- common.mk
+++ common.mk
@@ -272,6 +272,10 @@ PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-3.9.1-vendorcompat \
     libprotobuf-cpp-lite-3.9.1-vendorcompat
 
+# Remove packages
+PRODUCT_PACKAGES += \
+    RemovePkgs
+
 # Ril
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.4.vendor \
