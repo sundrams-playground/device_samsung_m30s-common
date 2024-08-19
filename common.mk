d7defb89b180e17cce2050390dc8378f89b27b31
xyz-sundram <sundrraam@gmail.com>
2024-08-19 12:37:32 +0530

    Revert "m30s-common: Use pixel thermal HAL directly"
    This reverts commit c048f683ee73bbcd1514464ea51b4d6e85946cae.

--- common.mk
+++ common.mk
@@ -284,8 +284,8 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal-service.pixel \
-    thermal_symlinks
+    android.hardware.thermal-service.samsung \
+    thermal_symlinks.samsung
 
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/thermal/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
