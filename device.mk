5fa662090fc4eb5977c3fc7fe4c1c74260dc0d6d
xyz-sundram <sundaramxd@outlook.com>
2026-01-16 07:47:27 +0000

    m30s: build fastbootd

--- device.mk
+++ device.mk
@@ -109,6 +109,11 @@ PRODUCT_PACKAGES += \
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+
+# Fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl:64 \
